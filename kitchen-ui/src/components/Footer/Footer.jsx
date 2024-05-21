aaaf3ef654f3809fc604c5addd272630b9a43b6a
Amrendra Kumar <amrendra.kumar@kapturecrm.com>
2024-05-21 12:40:18 +0530

    Revert "Revert "created indian kitchen ui""
    This reverts commit 745104b3592b8e92806cdd7c33bf98c18fe2f3ee.

--- kitchen-ui/src/components/Footer/Footer.jsx
+++ kitchen-ui/src/components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Footer() {
   return (
@@ -8,12 +8,14 @@ function Footer() {
         <div className="md:flex md:justify-between">
           <div className="mb-6 md:mb-0">
             <Link to="/" className="flex items-center">
-              <h6 className='size-10'>Kitchen</h6>
+              <h6 className="size-10">Kitchen</h6>
             </Link>
           </div>
           <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
             <div>
-              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">Resources</h2>
+              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">
+                Resources
+              </h2>
               <ul className="text-gray-500 font-medium">
                 <li className="mb-4">
                   <Link to="/home" className="hover:underline">
@@ -28,7 +30,9 @@ function Footer() {
               </ul>
             </div>
             <div>
-              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">Follow us</h2>
+              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">
+                Follow us
+              </h2>
               <ul className="text-gray-500 font-medium">
                 <li className="mb-4">
                   <a
@@ -48,7 +52,9 @@ function Footer() {
               </ul>
             </div>
             <div>
-              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">Legal</h2>
+              <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">
+                Legal
+              </h2>
               <ul className="text-gray-500 font-medium">
                 <li className="mb-4">
                   <Link to="#" className="hover:underline">
@@ -154,7 +160,6 @@ function Footer() {
         </div>
       </div>
     </footer>
-
   );
 }
 
